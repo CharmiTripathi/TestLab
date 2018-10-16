97bd010438cb688a217bb3e071cf846d40f6dc0f
CharmiTripathi <44186668+CharmiTripathi@users.noreply.github.com>
2018-10-16 10:01:14 +0530

    Update Insertionsort.c

--- Insertionsort.c
+++ Insertionsort.c
@@ -1,7 +1,7 @@
 #include <stdio.h> 
 #include <math.h> 
   
-/* Function to sort an array using insertion sort*/
+
 void insertionSort(int arr[], int n) 
 { 
    int i, key, j; 
@@ -10,9 +10,7 @@ void insertionSort(int arr[], int n)
        key = arr[i]; 
        j = i-1; 
   
-       /* Move elements of arr[0..i-1], that are 
-          greater than key, to one position ahead 
-          of their current position */
+       
        while (j >= 0 && arr[j] > key) 
        { 
            arr[j+1] = arr[j]; 
@@ -22,7 +20,7 @@ void insertionSort(int arr[], int n)
    } 
 } 
   
-// A utility function to print an array of size n 
+
 void printArray(int arr[], int n) 
 { 
 int i; 
@@ -33,7 +31,7 @@ int i;
   
   
   
-/* Driver program to test insertion sort */
+
 int main() 
 { 
     int arr[] = {12, 11, 13, 5, 6}; 
